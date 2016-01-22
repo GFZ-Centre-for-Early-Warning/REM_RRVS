69f89ec6789ed37672339e175f1ba88d3a7f8864
MWieland <mwieland@gfz-potsdam.de>
2016-01-22 14:27:51 +0100

    updated db scripts

--- rrvs_config.py
+++ rrvs_config.py
@@ -1,4 +1,4 @@
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost:5432/rrvstool_db_test'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost:5432/rrvstool_db'
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'development key wtf'
 DEBUG = True
