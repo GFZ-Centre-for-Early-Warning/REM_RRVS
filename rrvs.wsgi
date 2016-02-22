0a549f61ae16213b9a78fbce9443967f58dc46ef
mhaas <mhaas@gfz-potsdam.de>
2016-02-22 14:08:43 +0100

    some changes to wsgi integration

--- rrvs.wsgi
+++ rrvs.wsgi
@@ -1,11 +1,7 @@
-activate_this = '/srv/http/rrvs/venv/bin/activate_this.py'
-execfile(activate_this, dict(__file__=activate_this))
-
 import sys
 import logging
 logging.basicConfig(stream=sys.stderr)
-sys.path.append("/srv/http/")
-sys.path.append("/srv/http/rrvs/")
+sys.path.append("/var/www/html/rrvs")
 
 from webapp import app as application,db
 
--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
-from flask.ext.security import Security
+from flask.ext.security import Security,SQLAlchemyUserDatastore
 
 app = Flask(__name__)
 app.config.from_object('rrvs_config')
@@ -11,11 +11,17 @@ db = SQLAlchemy(app)
 import webapp.views
 db.init_app(app)
 
+#create login_manager
+login_manager = LoginManager()
+login_manager.init_app(app)
+#setup flask security
+user_datastore = SQLAlchemyUserDatastore(db, models.User, models.Role)
+security = Security(app, user_datastore)
 
 if __name__== "__main__":
+
     app.run(host="0.0.0.0")
 
     @login_manager.user_loader
     def load_user(user_id):
         return User.get(user_id)
-
