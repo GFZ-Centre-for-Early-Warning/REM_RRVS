de7395454ef02f89b844cde31d681706eda1ba9e
m-haas <mhaas@gfz-potsdam.de>
2017-04-12 19:18:32 +0200

    small fix in requirements

--- scripts/users.sql
+++ scripts/users.sql
@@ -1,6 +1,6 @@
 ﻿--add test users, roles and tasks
-INSERT INTO users.users(id, authenticated, name) VALUES (1, TRUE, 'Kerak engineer');
-INSERT INTO users.users(id, authenticated, name) VALUES (2, TRUE, 'Madaba engineer');
+INSERT INTO users.users(id, authenticated, name) VALUES (1, TRUE, 'Test');
+INSERT INTO users.users(id, authenticated, name) VALUES (2, TRUE, 'Test2');
 
 INSERT INTO users.roles(id, name) VALUES (1, 'public');
 INSERT INTO users.roles_users(user_id, role_id) VALUES (1, 1);
